4c071014bab5df6e63e8f1115c9719a306ab0705
leaf0165 <209923551+leaf0165@users.noreply.github.com>
2025-05-11 13:32:47 +0100

    Add resize grid button
    Users can create n*n grid from sizes 1 to 100

--- script.js
+++ script.js
@@ -1,26 +1,59 @@
 const container = document.querySelector(".container");
 
-//create 16x16 grid
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.setAttribute("class", "row");
+function resizeGrid(size) {
+    clearGrid();
 
-    for (let j = 0; j < 16; j++) {
-        tile = document.createElement("div");
-        tile.setAttribute("class", "tile");
-        row.appendChild(tile);
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
+    
+        for (let j = 0; j < size; j++) {
+            tile = document.createElement("div");
+            tile.setAttribute("class", "tile");
+            row.appendChild(tile);
+        }
+    
+        container.appendChild(row);
     }
 
-    container.appendChild(row);
+    setTileHover();
 }
 
-const tiles = document.querySelectorAll(".tile");
-tiles.forEach((tile) => {
-    tile.addEventListener("mouseenter", () => {
-        tile.classList.toggle("hover");
-    });
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
 
-    tile.addEventListener("mouseleave", () => {
-        tile.classList.toggle("hover");
-    })
+function setTileHover() {
+    const tiles = document.querySelectorAll(".tile");
+    tiles.forEach((tile) => {
+        tile.addEventListener("mouseenter", () => {
+            tile.classList.toggle("hover");
+        });
+
+        tile.addEventListener("mouseleave", () => {
+            tile.classList.toggle("hover");
+        })
+    });
+}
+
+const resizeGridButton = document.querySelector(".resize-grid");
+resizeGridButton.addEventListener("click", () => {
+    size = Number(prompt("Enter a whole number between 1 and 100"));
+
+    if (size < 1) {
+        size = 1;
+    } else if (size > 100) {
+        size = 100;
+    } else if (String(size) === "NaN") {
+        size = 16;
+    } else {
+        size = Math.round(size);
+    }
+
+    resizeGrid(size);
 });
+
+//start page with a 16x16 grid
+resizeGrid(16);
